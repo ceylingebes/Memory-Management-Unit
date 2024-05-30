2227b280b9a5ab4fbd77dcc91cc5370cf885b019
ceylin <110239708+ceylingebes@users.noreply.github.com>
2024-05-30 22:02:50 +0300

    implement procedure 1

---
+++ main.rkt
@@ -0,0 +1,19 @@
+;huriye ceylin gebes
+;2021400306
+;compiling: yes
+;complete: yes
+#lang racket
+
+(provide (all-defined-out))
+
+
+(define (binary_to_decimal binary) ; 3.1 DONE
+  (define binary-list (reverse (string->list binary))) 
+  (define (char-to-decimal c)
+    (if (char=? c #\1) 1 0))
+  (foldl (lambda (digit acc)
+           (+ acc (* (car digit) (expt 2 (cdr digit)))))
+         0
+         (map (lambda (x y) (cons (char-to-decimal x) y))
+              binary-list
+              (range 0 (length binary-list)))))
